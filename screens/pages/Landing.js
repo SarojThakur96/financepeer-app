8db3f635df6c4e2fe9e1fb819ddf109efaa9c54f
saroj <saroj@distronix.in>
2022-09-02 16:38:26 +0530

    image crousal added

--- screens/pages/Landing.js
+++ screens/pages/Landing.js
@@ -5,20 +5,110 @@ import {
   TouchableOpacity,
   ImageBackground,
   Image,
+  FlatList,
+  useWindowDimensions,
+  Animated,
 } from 'react-native';
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import {BellIcon} from 'react-native-heroicons/solid';
 import Header from '../../components/Header';
-import ImageSlider from 'react-native-image-slider';
 import {TouchableHighlight} from 'react-native-gesture-handler';
 
 const Landing = () => {
-  const images = [
-    'https://placeimg.com/640/640/nature',
-    'https://placeimg.com/640/640/people',
-    'https://placeimg.com/640/640/animals',
-    'https://placeimg.com/640/640/beer',
+  const {width} = useWindowDimensions();
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const slidesRef = useRef(null);
+  const viewableItemsChanged = useRef(({viewableItems}) => {
+    setCurrentIndex(viewableItems[0]?.index);
+  }).current;
+  const viewConfig = useRef({viewAreaCoveragePercentThreshold: 50}).current;
+
+  const data = [
+    {
+      id: '1',
+      image: 'https://placeimg.com/640/640/nature',
+    },
+    {
+      id: '2',
+      image:
+        'https://sm.mashable.com/mashable_sea/photo/default/gophers-goofing_trax.jpg',
+    },
+    {
+      id: '3',
+      image:
+        'https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg?w=2000',
+    },
+    {
+      id: '4',
+      image:
+        'https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg',
+    },
+    {
+      id: '5',
+      image:
+        'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+    },
   ];
+  const RenderItem = ({item}) => {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: width,
+        }}
+      >
+        <Image
+          source={{uri: item.image}}
+          style={{
+            width: width,
+            height: 300,
+            resizeMode: 'contain',
+            justifyContent: 'center',
+          }}
+        />
+        <View
+          style={{
+            flexDirection: 'row',
+            height: 64,
+            position: 'absolute',
+            bottom: 0,
+          }}
+        >
+          {data.map((_, i) => {
+            const inputRange = [(i - 1) * width, i * width, (i + 1) * width];
+
+            const opacity = scrollX.interpolate({
+              inputRange,
+              outputRange: [0.7, 1, 0.7],
+              extrapolate: 'clamp',
+            });
+            const backgroundColor = scrollX.interpolate({
+              inputRange,
+              outputRange: ['#ffffff', '#493d8a', '#ffffff'],
+              extrapolate: 'clamp',
+            });
+
+            return (
+              <Animated.View
+                style={{
+                  height: 10,
+                  width: 10,
+                  borderRadius: 5,
+                  backgroundColor,
+                  marginHorizontal: 8,
+                  opacity,
+                }}
+                key={i.toString()}
+              />
+            );
+          })}
+        </View>
+      </View>
+    );
+  };
 
   return (
     <View style={{flex: 1}}>
@@ -31,35 +121,29 @@ const Landing = () => {
         style={{flex: 1}}
       >
         <Header />
-        {/* <ImageSlider
-          loopBothSides
-          autoPlayWithInterval={3000}
-          images={images}
-          customSlide={({index, item, style, width}) => (
-            // It's important to put style here because it's got offset inside
-            <View key={index} style={[style, styles.customSlide]}>
-              <Image source={{uri: item}} style={styles.customImage} />
-            </View>
-          )}
-          customButtons={(position, move) => (
-            <View style={styles.buttons}>
-              {images.map((image, index) => {
-                return (
-                  <TouchableHighlight
-                    key={index}
-                    underlayColor="#ccc"
-                    onPress={() => move(index)}
-                    style={styles.button}
-                  >
-                    <Text style={position === index && styles.buttonSelected}>
-                      {index + 1}
-                    </Text>
-                  </TouchableHighlight>
-                );
-              })}
-            </View>
-          )}
-        /> */}
+        <View
+          style={{
+            marginVertical: 150,
+          }}
+        >
+          <FlatList
+            data={data}
+            renderItem={({item}) => <RenderItem item={item} />}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            pagingEnabled
+            bounces={false}
+            keyExtractor={item => item.id}
+            onScroll={Animated.event(
+              [{nativeEvent: {contentOffset: {x: scrollX}}}],
+              {useNativeDriver: false},
+            )}
+            onViewableItemsChanged={viewableItemsChanged}
+            viewabilityConfig={viewConfig}
+            ref={slidesRef}
+            scrollEventThrottle={32}
+          />
+        </View>
       </ImageBackground>
     </View>
   );
